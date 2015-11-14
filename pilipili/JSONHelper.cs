dfe626f58e93a1f7cb947910f88c135d6a3238f0
Lu Hao <Hale_Lu@nuaa.edu.cn>
2015-11-15 02:22:36 +0800

    注释+解决timer的自动回收问题

--- pilipili/JSONHelper.cs
+++ pilipili/JSONHelper.cs
@@ -1,22 +1,23 @@
-﻿using System.Runtime.Serialization;
+﻿using System.IO;
 using System.Runtime.Serialization.Json;
-using System.IO;
 using System.Text;
+
 public class JSONHelper
 {
     public static string Serialize<T>(T obj)
     {
-        DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
-        MemoryStream ms = new MemoryStream();
+        var serializer = new DataContractJsonSerializer(obj.GetType());
+        var ms = new MemoryStream();
         serializer.WriteObject(ms, obj);
-        string retVal = Encoding.Default.GetString(ms.ToArray());
+        var retVal = Encoding.Default.GetString(ms.ToArray());
         return retVal;
     }
+
     public static T Deserialize<T>(string json)
     {
-        MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
-        DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
-        T obj = (T)serializer.ReadObject(ms);
+        var ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
+        var serializer = new DataContractJsonSerializer(typeof (T));
+        var obj = (T) serializer.ReadObject(ms);
         ms.Close();
         return obj;
     }
--- pilipili/MainWindow.xaml.cs
+++ pilipili/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -17,6 +16,7 @@ namespace pilipili
     /// </summary>
     public partial class MainWindow : Window
     {
+        //颜色列表
         private static readonly SolidColorBrush[] colorList =
         {
             Brushes.AliceBlue, Brushes.Aqua, Brushes.Black, Brushes.Blue,
@@ -24,38 +24,50 @@ namespace pilipili
             Brushes.White, Brushes.White, Brushes.White, Brushes.White, Brushes.White
         };
 
+        //弹幕标签列表（循环队列）
         private static readonly Label[] labelList = new Label[1024];
+        //初始化布局
         private readonly Grid g = new Grid {Margin = new Thickness(0, 0, 0, 0)};
-        private int id = 0;
-
+        //随机数生成器
         private readonly Random rd = new Random();
+        
+        //发送GET请求的定时器
         private readonly Timer timer_get = new Timer();
+        //移动弹幕的定时器
         private readonly Timer timer_move = new Timer();
 
+        //循环队列的头和尾序号
         private int begin;
         private int end;
+
+        //当前已从服务器接收到的弹幕id
+        private int id;
 
         public MainWindow()
         {
             InitializeComponent();
+        }
 
+        //初始化两个计时器
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
             timer_move.Elapsed += MoveLabelEvent;
             timer_move.Interval = 10;
             timer_move.Start();
             timer_move.Enabled = true;
 
             timer_get.Elapsed += SendGetEvent;
+            timer_get.Elapsed += GenMessageEvent;
             timer_get.Interval = 1000;
             timer_get.Start();
             timer_get.Enabled = true;
-        }
-
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
             AddChild(g);
+
+            GC.KeepAlive(timer_move);
+            GC.KeepAlive(timer_get);
         }
 
+        //弹幕移动
         public void MoveLabelEvent(object sender, ElapsedEventArgs e)
         {
             Dispatcher.Invoke(
@@ -77,11 +89,37 @@ namespace pilipili
                 );
         }
 
+        //弹幕生成器（测试用）
+        private void GenMessageEvent(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(
+                delegate
+                {
+                    var l = new Label
+                    {
+                        Content = rd.NextDouble(),
+                        //Content = e.SignalTime.ToString(),
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Top,
+                        Margin = new Thickness(-Width, rd.NextDouble()*Height, 0, 0),
+                        UseLayoutRounding = true,
+                        Foreground = colorList[rd.Next()%16],
+                        FontSize = 30
+                    };
+                    labelList[end] = l;
+                    end++;
+                    end %= 1024;
+                    g.Children.Add(l);
+                }
+                );
+        }
+
+        //发送GET请求并用返回值生成弹幕
         private void SendGetEvent(object sender, ElapsedEventArgs e)
         {
             var json = HttpGet("http://2.countdown2014.sinaapp.com/index.php", "id=" + id);
-            JObject jo = (JObject)JsonConvert.DeserializeObject(json);
-            JArray res = JArray.Parse(jo["data"].ToString());
+            var jo = (JObject) JsonConvert.DeserializeObject(json);
+            var res = JArray.Parse(jo["data"].ToString());
             foreach (var message in res)
             {
                 Dispatcher.Invoke(
@@ -91,8 +129,8 @@ namespace pilipili
                         {
                             Content = message["content"],
                             HorizontalAlignment = HorizontalAlignment.Left,
-                            Margin = new Thickness(-Width, rd.NextDouble()*Height, 0, 0),
                             VerticalAlignment = VerticalAlignment.Top,
+                            Margin = new Thickness(-Width, rd.NextDouble()*Height, 0, 0),
                             UseLayoutRounding = true,
                             Foreground = colorList[rd.Next()%16],
                             FontSize = 30
@@ -103,10 +141,11 @@ namespace pilipili
                         g.Children.Add(l);
                     }
                     );
-                id = id > Int32.Parse(message["id"].ToString()) ? id : Int32.Parse(message["id"].ToString());
+                id = id > int.Parse(message["id"].ToString()) ? id : int.Parse(message["id"].ToString());
             }
         }
 
+        //GET方法模板
         public string HttpGet(string Url, string postDataStr)
         {
             var request = (HttpWebRequest) WebRequest.Create(Url + (postDataStr == "" ? "" : "?") + postDataStr);
